2213c4403d2c65f6648f00151e15d61c9ee6ac2e
caspervdw <caspervdw@gmail.com>
2016-03-08 12:07:29 +0100

    Configure versioneer

--- circletracking/__init__.py
+++ circletracking/__init__.py
@@ -4,3 +4,7 @@ from .radialprofile import (find_ellipse, find_ellipsoid, refine_ellipse,
 from .algebraic import (ellipse_grid, ellipsoid_grid,
                         fit_ellipse, fit_ellipsoid)
 from .artificial import gen_artificial_ellipsoid
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
